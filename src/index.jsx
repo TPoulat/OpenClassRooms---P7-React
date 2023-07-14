7bf9ca494b01a50293499cee9f4f088ab907d2b2
Tristan Poulat <poulat.tristan@gmail.com>
2023-07-14 14:16:39 +0200

    mise en page index

--- src/index.jsx
+++ src/index.jsx
@@ -9,7 +9,6 @@ import "./styles/index.scss";
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
