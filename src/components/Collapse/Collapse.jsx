b595ff8cbb21c5fd0d709d78751acd987f0697c3
Tristan Poulat <poulat.tristan@gmail.com>
2023-07-14 14:02:14 +0200

    collapse et page about

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,51 +1,52 @@
-// import { useState } from 'react';
-// import PropTypes from 'prop-types';
-// import upArrow from '../../assets/img/upArrow.png';
-// import downArrow from '../../assets/img/downArrow.png';
+import React from "react";
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import upArrow from '../../assets/img/upArrow.png';
+import downArrow from '../../assets/img/downArrow.png';
 
-// function Collapse(props) {
-//   const [toggle, setToggle] = useState(false);
+function Collapse(props) {
+  const [toggle, setToggle] = useState(false);
 
-//   const toggleState = () => {
-//     setToggle(!toggle);
-//   };
+  const toggleState = () => {
+    setToggle(!toggle);
+  };
 
-//   return (
-//     <section className="collapse">
-//       <div className="collapse_visible" onClick={toggleState}>
-//         <h2 className="collapse_title">{props.collapseTitle}</h2>
-//         {toggle ? (
-//           <img className="UpArrow" src={upArrow} alt="Up Arrow" />
-//         ) : (
-//           <img className="DownArrow" src={downArrow} alt="Down Arrow" />
-//         )}
-//       </div>
-//       <div className={`collapse_hidden ${toggle ? 'is-visible' : ''}`} data-isaboutpage={props.isAboutPage ? 'true' : 'false'}>
-//         {Array.isArray(props.collapseText) ? (
-//           <ul>
-//             {props.collapseText.map((text, index) => (  /* Boucle pour afficher chaque élément du tableau collapseText */
-//               <li key={index}>{text}</li>
-//             ))}
-//           </ul>
-//         ) : (
-//           <p>{props.collapseText}</p>
-//         )}
-//       </div>
-//     </section>
-//   );
-// }
+  return (
+    <section className="collapse">
+      <div className="collapse_visible" onClick={toggleState}>
+        <h2 className="collapse_title">{props.collapseTitle}</h2>
+        {toggle ? (
+          <img className="UpArrow" src={upArrow} alt="Up Arrow" />
+        ) : (
+          <img className="DownArrow" src={downArrow} alt="Down Arrow" />
+        )}
+      </div>
+      <div className={`collapse_hidden ${toggle ? 'is-visible' : ''}`} data-isaboutpage={props.isAboutPage ? 'true' : 'false'}>
+        {Array.isArray(props.collapseText) ? (
+          <ul>
+            {props.collapseText.map((text, index) => (  /* Boucle pour afficher chaque élément du tableau collapseText */
+              <li key={index}>{text}</li>
+            ))}
+          </ul>
+        ) : (
+          <p>{props.collapseText}</p>
+        )}
+      </div>
+    </section>
+  );
+}
 
-// Collapse.propTypes = {
-//   isAboutPage: PropTypes.bool.isRequired,
-//   collapseTitle: PropTypes.string.isRequired,
-//   collapseText: PropTypes.oneOfType([
-//     PropTypes.string,
-//     PropTypes.arrayOf(PropTypes.string),
-//   ]).isRequired,
-// };
+Collapse.propTypes = {
+  isAboutPage: PropTypes.bool.isRequired,
+  collapseTitle: PropTypes.string.isRequired,
+  collapseText: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.arrayOf(PropTypes.string),
+  ]).isRequired,
+};
 
-// Collapse.defaultProps = {
-//   isAboutPage: false,
-// };
+Collapse.defaultProps = {
+  isAboutPage: false,
+};
 
-// export default Collapse;
+export default Collapse;
