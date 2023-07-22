a1bee03a8c8498d5f4893a6d718e6e029308b2fe
Tristan Poulat <poulat.tristan@gmail.com>
2023-07-22 14:06:21 +0200

    correction

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,9 +3,6 @@ import Banner from "../../components/Banner/Banner";
 import image from "../../assets/img/banner.png";
 import CardsContainer from "../../components/CardsContainer/CardsContainer";
 
-
-
-
 function Home() {
     return (
         <main>
